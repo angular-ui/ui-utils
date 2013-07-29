209c0f5acfefc6e3b2aa9d5a4ec674ca25c9b926
Douglas Duteil <douglasduteil@gmail.com>
2013-07-29 18:42:54 +0200

    [M] Doc refacto

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -10,9 +10,9 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'components/jquery/jquery.js',
-  'components/angular/angular.js',
-  'components/angular-mocks/angular-mocks.js',
+  'bower_components/jquery/jquery.js',
+  'bower_components/angular/angular.js',
+  'bower_components/angular-mocks/angular-mocks.js',
   'modules/*/*.js',
   'modules/*/test/*Spec.js'
 ];
