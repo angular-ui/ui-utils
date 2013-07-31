bc781fe94286376b571bf3dfe68175139183f1ee
Douglas Duteil <douglasduteil@gmail.com>
2013-08-01 00:21:44 +0200

    [M] Reformat test code

--- modules/include/test/includeSpec.js
+++ modules/include/test/includeSpec.js
@@ -1,41 +1,34 @@
 // modeled after: angular.js/test/ng/directive/ngIncludeSpec.js
 describe('uiInclude', function() {
-  var element;
+  var scope, $compile, $templateCache, element;
 
   afterEach(function() {
     element.remove();
   });
 
   beforeEach(module('ui.include'));
+  beforeEach(inject(function (_$rootScope_, _$compile_, _$templateCache_ ) {
+    scope = _$rootScope_.$new();
+    $compile = _$compile_;
+    $templateCache = _$templateCache_;
+  }));
 
   function putIntoCache(url, content) {
-    return function($templateCache) {
-      $templateCache.put(url, [200, content, {}]);
-    };
+    $templateCache.put(url, [200, content, {}]);
   }
 
-  it('should include on external file', inject(putIntoCache('myUrl', '{{name}}'),
-      function($rootScope, $compile) {
-    element = angular.element('<div ui-include src="url"></div>');
-    angular.element(document.body).append(element);
-    element = $compile(element)($rootScope);
-    $rootScope.name = 'misko';
-    $rootScope.url = 'myUrl';
-    $rootScope.$digest();
+  it('should include on external file', function () {
+    putIntoCache('myUrl', '{{name}}');
+    element = $compile('<div ui-include src="\'myUrl\'"></div>')(scope);
+    scope.$apply("name = 'misko'");
     expect(element.text()).toEqual('misko');
-    angular.element(document.body).html('');
-  }));
+  });
 
-  it('should work with a fragment selector', inject(putIntoCache('myUrl', '<a>foo {{name}}</a><b>bar {{name}}</b><c>baz {{name}}</c>'),
-      function($rootScope, $compile) {
-    element = angular.element('<div ui-include="url" fragment="\'b\'"></div>');
-    angular.element(document.body).append(element);
-    element = $compile(element)($rootScope);
-    $rootScope.name = 'misko';
-    $rootScope.url = 'myUrl';
-    $rootScope.$digest();
+  it('should work with a fragment selector', function () {
+    putIntoCache('myUrl', '<a>foo {{name}}</a><b>bar {{name}}</b><c>baz {{name}}</c>');
+    element = $compile('<div ui-include src="\'myUrl\'" fragment="\'b\'"></div>')(scope);
+    scope.$apply("name = 'misko'");
     expect(element.text()).toEqual('bar misko');
-    angular.element(document.body).html('');
-  }));
+  });
 
 });
