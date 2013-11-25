fbef0eeb45ca83cdc93d318584d65b11fdcf7be4
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2013-11-25 09:02:12 +0000

    Travis commit : build 204

--- build/ui-utils.js
+++ build/ui-utils.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.0.4 - 2013-11-24
+ * @version v0.0.4 - 2013-11-25
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
