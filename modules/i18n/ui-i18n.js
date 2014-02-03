8327d7068642a62c53eac7561c3bfcb525b102b9
Tim Sweet <timothyswt@gmail.com>
2014-02-03 10:07:58 -0700

    forgot to check in observable support last night

--- modules/i18n/ui-i18n.js
+++ modules/i18n/ui-i18n.js
@@ -1,32 +1,23 @@
 /**
- * Created by Tim on 2/1/14.
+ * ui-i18n Created by Tim Sweet on 2/1/14.
+ * https://github.com/timothyswt
+ * MIT License
  */
-(function(){
+(function(deepExtend){
     'use strict';
-    // adding deep copy method until angularjs supports deep copy like everyone else.
-    // https://github.com/angular/angular.js/pull/5059
-    function deepExtend(destination, source) {
-        for (var property in source) {
-            if (source[property] && source[property].constructor &&
-                source[property].constructor === Object) {
-                destination[property] = destination[property] || {};
-                /* jshint ignore:start */
-                arguments.callee(destination[property], source[property]);
-                /* jshint ignore:end */
-            } else {
-                destination[property] = source[property];
-            }
-        }
-        return destination;
-    }
-    'use strict';
-    var MISSING = '[MISSING]: ';
-    var uiI18n = angular.module('ui.i18n', []);
+    var MISSING = '[MISSING]: ',
+        UPDATE_EVENT = '$uiI18n',
+        FILTER_ALIASES = ['t', 'translate'],
+        DIRECTIVE_ALIASES = ['uiT', 'uiTranslate'],
+        LOCALE_DIRECTIVE_ALIAS = 'uiI18n',
+        // default to english
+        DEFAULT_LANG = 'en-US',
+        langCache = {
+            _langs: {},
+            current: null
+        },
+        uiI18n = angular.module('ui.i18n', []);
 
-    var langCache = {
-        _langs: {},
-        current: null
-    };
     langCache.get = function(lang){
         return langCache._langs[lang.toLowerCase()];
     };
@@ -45,7 +36,7 @@
     uiI18n._cache = langCache;
     uiI18n.$broadcast = function(lang){
         if (lang && this.$root){
-            uiI18n.$root.$broadcast('$uiI18n', lang);
+            uiI18n.$root.$broadcast(UPDATE_EVENT, lang);
         }
     };
     uiI18n.add =  function(langs, strings){
@@ -66,45 +57,67 @@
         }
     };
 
-    uiI18n.directive('uiI18n',function() {
+    var localeDirective = function() {
         return {
-            link: function($scope, $elm, $attrs) {
-                if (!uiI18n.$root) uiI18n.$root = $scope.$root;
-                // check for watchable property
-                var lang = $scope.$eval($attrs.uiI18n);
-                if (lang){
-                    $scope.$watch($attrs.uiI18n, uiI18n.set);
-                } else {
-                    // fall back to the string value
-                    lang = $attrs.uiI18n;
+            compile: function(){
+                return {
+                    pre: function($scope, $elm, $attrs) {
+                        var alias = LOCALE_DIRECTIVE_ALIAS;
+                        if (!uiI18n.$root) uiI18n.$root = $scope.$root;
+                        // check for watchable property
+                        var lang = $scope.$eval($attrs[alias]);
+                        if (lang){
+                            $scope.$watch($attrs[alias], uiI18n.set);
+                        } else if ($attrs.$$observers){
+                            $scope.$on('$destroy', $attrs.$observe(alias, uiI18n.set));
+                        } else {
+                            // fall back to the string value
+                            lang = $attrs[alias];
+                        }
+                        uiI18n.set(lang || DEFAULT_LANG);
+                    }
                 }
-                uiI18n.set(lang);
             }
         };
-    });
+    };
+    uiI18n.directive(LOCALE_DIRECTIVE_ALIAS, localeDirective);
 
     // directive syntax
     var uitDirective = function($parse) {
         return {
             restrict: 'EA',
             compile: function(){
-                return function($scope, $elm, $attrs) {
-                    if (!uiI18n.$root) uiI18n.$root = $scope.$root;
-                    var token = $attrs.uiT || $attrs.uiTranslate || $elm.html();
-                    var getter = $parse(token);
-                    var missing = MISSING + token;
-
-                    var listener = $scope.$on('$uiI18n', function(evt, lang){
-                        // set text based on i18n current language
-                        $elm.html(getter(langCache.get(lang)) || missing);
-                    });
-                    $scope.$on('$destroy', listener);
-                };
+                return {
+                    pre: function($scope, $elm, $attrs) {
+                        if (!uiI18n.$root) uiI18n.$root = $scope.$root;
+                        var alias1 = DIRECTIVE_ALIASES[0],
+                            alias2 = DIRECTIVE_ALIASES[1];
+                        var token = $attrs[alias1] || $attrs[alias2] || $elm.html();
+                        var missing = MISSING + token;
+                        var observer;
+                        if ($attrs.$$observers){
+                            var prop = $attrs[alias1] ? alias1 : alias2;
+                            observer = $attrs.$observe(prop, function(result){
+                                if (result){
+                                    $elm.html($parse(result)(langCache.getCurrent()) || missing);
+                                }
+                            });
+                        }
+                        var getter = $parse(token);
+                        var listener = $scope.$on(UPDATE_EVENT, function(evt, lang){
+                            if (observer){
+                                observer($attrs[alias1] || $attrs[alias2]);
+                            } else {
+                                // set text based on i18n current language
+                                $elm.html(getter(langCache.get(lang)) || missing);
+                            }
+                        });
+                        $scope.$on('$destroy', listener);
+                    }
+                }
             }
         };
     };
-    uiI18n.directive('uiT',['$parse', uitDirective]);
-    uiI18n.directive('uiTranslate',['$parse', uitDirective]);
 
     // optional filter syntax
     var uitFilter = function($parse) {
@@ -114,6 +127,24 @@
             return getter(langCache.getCurrent()) || MISSING + data;
         };
     };
-    uiI18n.filter('t', ['$parse', uitFilter]);
-    uiI18n.filter('translate', ['$parse', uitFilter]);
-})();
+
+    angular.forEach(DIRECTIVE_ALIASES, function(alias){
+        uiI18n.directive(alias,['$parse', uitDirective]);
+    });
+    angular.forEach(FILTER_ALIASES, function(alias){
+        uiI18n.filter(alias,['$parse', uitFilter]);
+    });
+})(function(destination, source) {
+    // adding deep copy method until angularjs supports deep copy like everyone else.
+    // https://github.com/angular/angular.js/pull/5059
+    for (var property in source) {
+        if (source[property] && source[property].constructor &&
+            source[property].constructor === Object) {
+            destination[property] = destination[property] || {};
+            arguments.callee(destination[property], source[property]);
+        } else {
+            destination[property] = source[property];
+        }
+    }
+    return destination;
+});
