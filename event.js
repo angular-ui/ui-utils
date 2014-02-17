73c2f69b0f8fc2fd15f006c4b76dea873ee1a590
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-02-17 23:31:17 +0000

    Travis commit : build 271

--- event.js
+++ event.js
@@ -1,4 +1,14 @@
 'use strict';
+/**
+ * General-purpose Event binding. Bind any event not natively supported by Angular
+ * Pass an object with keynames for events to ui-event
+ * Allows $event object and $params object to be passed
+ *
+ * @example <input ui-event="{ focus : 'counter++', blur : 'someCallback()' }">
+ * @example <input ui-event="{ myCustomEvent : 'myEventHandler($event, $params)'}">
+ *
+ * @param ui-event {string|object literal} The event to bind to as a string or a hash of events with their callbacks
+ */
 angular.module('ui.event', []).directive('uiEvent', [
   '$parse',
   function ($parse) {
@@ -8,6 +18,7 @@ angular.module('ui.event', []).directive('uiEvent', [
         var fn = $parse(uiEvent);
         elm.bind(eventName, function (evt) {
           var params = Array.prototype.slice.call(arguments);
+          //Take out first paramater (event object);
           params = params.splice(1);
           fn($scope, {
             $event: evt,
