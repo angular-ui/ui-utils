f03a858a922a1da549e42e92d77d14ccd94fe86f
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-02-17 23:31:44 +0000

    Travis commit : build 271

--- keypress.js
+++ keypress.js
@@ -25,6 +25,7 @@ angular.module('ui.keypress', []).factory('keypressHelper', [
     return function (mode, scope, elm, attrs) {
       var params, combinations = [];
       params = scope.$eval(attrs['ui' + capitaliseFirstLetter(mode)]);
+      // Prepare combinations for simple checking
       angular.forEach(params, function (v, k) {
         var combination, expression;
         expression = $parse(v);
@@ -39,15 +40,19 @@ angular.module('ui.keypress', []).factory('keypressHelper', [
           combinations.push(combination);
         });
       });
+      // Check only matching of pressed keys one of the conditions
       elm.bind(mode, function (event) {
+        // No need to do that inside the cycle
         var metaPressed = !!(event.metaKey && !event.ctrlKey);
         var altPressed = !!event.altKey;
         var ctrlPressed = !!event.ctrlKey;
         var shiftPressed = !!event.shiftKey;
         var keyCode = event.keyCode;
+        // normalize keycodes
         if (mode === 'keypress' && !shiftPressed && keyCode >= 97 && keyCode <= 122) {
           keyCode = keyCode - 32;
         }
+        // Iterate over prepared combinations
         angular.forEach(combinations, function (combination) {
           var mainKeyPressed = combination.keys[keysByCode[keyCode]] || combination.keys[keyCode.toString()];
           var metaRequired = !!combination.keys.meta;
@@ -55,6 +60,7 @@ angular.module('ui.keypress', []).factory('keypressHelper', [
           var ctrlRequired = !!combination.keys.ctrl;
           var shiftRequired = !!combination.keys.shift;
           if (mainKeyPressed && metaRequired === metaPressed && altRequired === altPressed && ctrlRequired === ctrlPressed && shiftRequired === shiftPressed) {
+            // Run the function
             scope.$apply(function () {
               combination.expression(scope, { '$event': event });
             });
@@ -64,6 +70,11 @@ angular.module('ui.keypress', []).factory('keypressHelper', [
     };
   }
 ]);
+/**
+ * Bind one or more handlers to particular keys or their combination
+ * @param hash {mixed} keyBindings Can be an object or string where keybinding expression of keys or keys combinations and AngularJS Exspressions are set. Object syntax: "{ keys1: expression1 [, keys2: expression2 [ , ... ]]}". String syntax: ""expression1 on keys1 [ and expression2 on keys2 [ and ... ]]"". Expression is an AngularJS Expression, and key(s) are dash-separated combinations of keys and modifiers (one or many, if any. Order does not matter). Supported modifiers are 'ctrl', 'shift', 'alt' and key can be used either via its keyCode (13 for Return) or name. Named keys are 'backspace', 'tab', 'enter', 'esc', 'space', 'pageup', 'pagedown', 'end', 'home', 'left', 'up', 'right', 'down', 'insert', 'delete'.
+ * @example <input ui-keypress="{enter:'x = 1', 'ctrl-shift-space':'foo()', 'shift-13':'bar()'}" /> <input ui-keypress="foo = 2 on ctrl-13 and bar('hello') on shift-esc" />
+ **/
 angular.module('ui.keypress').directive('uiKeydown', [
   'keypressHelper',
   function (keypressHelper) {
