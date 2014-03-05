e36d4220eba8d607281aeae9c945c511594e603c
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-03-05 00:03:19 +0000

    Travis commit : build 274

--- alias.min.js
+++ alias.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.1.1 - 2014-02-17
+ * @version v0.1.1 - 2014-03-05
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
