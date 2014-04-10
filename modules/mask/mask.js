53752bfb8e9284e8e49ad77a7edae4eefb86a12a
miticv <miticv@gmail.com>
2014-04-10 10:56:31 -0400

    fixes IE bug: on TAB text should be highlighted
    Only on IE - input text with mask does not get highlighted when receives focus on TAB.
    This line fixes that.

--- modules/mask/mask.js
+++ modules/mask/mask.js
@@ -323,6 +323,9 @@ angular.module('ui.mask', [])
             var eventWhich = e.which,
               eventType = e.type;
 
+            //fixes IE bug: on TAB text should be highlighted
+            if (eventWhich === 9) { this.select(); return; }
+
             // Prevent shift and ctrl from mucking with old values
             if (eventWhich === 16 || eventWhich === 91) { return;}
 
