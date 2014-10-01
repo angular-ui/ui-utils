427f615d2f1e12cec02a3dab584a6d53c5683337
Paulo Cesar <email@pocesar.e4ward.com>
2014-10-01 09:53:42 -0300

    reset karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -17,8 +17,8 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'modules/mask/*.js',
-      'modules/mask/test/*Spec.js'
+      'modules/*/*.js',
+      'modules/*/test/*Spec.js'
     ],
 
 
