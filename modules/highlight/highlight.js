a2a4a3b988c6ffe4398fc87182005d771e3ec19e
Kaveh Shirgir <kaveh@Kavehs-MacBook-Pro.local>
2014-11-03 11:19:31 +1100

    escape search string from regular expression specific characters

--- modules/highlight/highlight.js
+++ modules/highlight/highlight.js
@@ -7,6 +7,10 @@
  * @param [caseSensitive] {boolean} optional boolean to use case-sensitive searching
  */
 angular.module('ui.highlight',[]).filter('highlight', function () {
+  function escapeRegExp(string){
+    return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
+  }
+
   return function (text, search, caseSensitive) {
     if (text && (search || angular.isNumber(search))) {
       text = text.toString();
@@ -14,6 +18,7 @@ angular.module('ui.highlight',[]).filter('highlight', function () {
       if (caseSensitive) {
         return text.split(search).join('<span class="ui-match">' + search + '</span>');
       } else {
+        search = escapeRegExp(search);
         return text.replace(new RegExp(search, 'gi'), '<span class="ui-match">$&</span>');
       }
     } else {
