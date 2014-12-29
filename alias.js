280e8988f006dbea24343c7058a78ca1511a49e1
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-12-29 07:28:10 +0000

    Travis commit : build 401

--- alias.js
+++ alias.js
@@ -1,8 +1,8 @@
-'use strict';
 angular.module('ui.alias', []).config([
   '$compileProvider',
   'uiAliasConfig',
   function ($compileProvider, uiAliasConfig) {
+    'use strict';
     uiAliasConfig = uiAliasConfig || {};
     angular.forEach(uiAliasConfig, function (config, alias) {
       if (angular.isString(config)) {
