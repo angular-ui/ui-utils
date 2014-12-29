592abe88079c674914bd11d4244ead90b0476f17
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-12-29 07:28:41 +0000

    Travis commit : build 401

--- uploader.js
+++ uploader.js
@@ -1,4 +1,3 @@
-'use strict';
 /*
  * Author: Remy Alain Ticona Carbajal http://realtica.org
  * Description: The main objective of ng-uploader is to have a user control,
@@ -8,6 +7,7 @@
 angular.module('ui.uploader', []).service('uiUploader', uiUploader);
 uiUploader.$inject = ['$log'];
 function uiUploader($log) {
+  'use strict';
   /*jshint validthis: true */
   var self = this;
   self.files = [];
