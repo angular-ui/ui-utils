323f6bd0775a68628e2052bcd96ac18b9b6cfefc
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2014-12-29 07:28:39 +0000

    Travis commit : build 401

--- unique.js
+++ unique.js
@@ -1,4 +1,3 @@
-'use strict';
 /**
  * Filters out all duplicate items from an array by checking the specified key
  * @param [key] {string} the name of the attribute of each object to compare for uniqueness
@@ -9,6 +8,7 @@
 angular.module('ui.unique', []).filter('unique', [
   '$parse',
   function ($parse) {
+    'use strict';
     return function (items, filterOn) {
       if (filterOn === false) {
         return items;
