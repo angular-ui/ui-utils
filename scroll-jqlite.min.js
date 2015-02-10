7b1c7159856682fe6d234b76cfd0bb6302725375
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-02-10 19:06:26 +0000

    Travis commit : build 416

--- scroll-jqlite.min.js
+++ scroll-jqlite.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.2.1 - 2015-02-05
+ * @version v0.2.1 - 2015-02-10
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
