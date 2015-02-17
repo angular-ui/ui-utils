e1513e03dc4c30e020194f7eafae67dfb5a0be8c
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-02-17 06:59:07 +0000

    Travis commit : build 418

--- scrollfix.js
+++ scrollfix.js
@@ -18,23 +18,28 @@ angular.module('ui.scrollfix', []).directive('uiScrollfix', [
     return {
       require: '^?uiScrollfixTarget',
       link: function (scope, elm, attrs, uiScrollfixTarget) {
-        var top = elm[0].offsetTop, $target = uiScrollfixTarget && uiScrollfixTarget.$element || angular.element($window);
+        var absolute = true, shift = 0, fixLimit, $target = uiScrollfixTarget && uiScrollfixTarget.$element || angular.element($window);
         if (!attrs.uiScrollfix) {
-          attrs.uiScrollfix = top;
+          absolute = false;
         } else if (typeof attrs.uiScrollfix === 'string') {
           // charAt is generally faster than indexOf: http://jsperf.com/indexof-vs-charat
           if (attrs.uiScrollfix.charAt(0) === '-') {
-            attrs.uiScrollfix = top - parseFloat(attrs.uiScrollfix.substr(1));
+            absolute = false;
+            shift = -parseFloat(attrs.uiScrollfix.substr(1));
           } else if (attrs.uiScrollfix.charAt(0) === '+') {
-            attrs.uiScrollfix = top + parseFloat(attrs.uiScrollfix.substr(1));
+            absolute = false;
+            shift = parseFloat(attrs.uiScrollfix.substr(1));
           }
         }
+        fixLimit = absolute ? attrs.uiScrollfix : elm[0].offsetTop + shift;
         function onScroll() {
+          var limit = absolute ? attrs.uiScrollfix : elm[0].offsetTop + shift;
           // if pageYOffset is defined use it, otherwise use other crap for IE
           var offset = uiScrollfixTarget ? $target[0].scrollTop : getWindowScrollTop();
-          if (!elm.hasClass('ui-scrollfix') && offset > attrs.uiScrollfix) {
+          if (!elm.hasClass('ui-scrollfix') && offset > limit) {
             elm.addClass('ui-scrollfix');
-          } else if (elm.hasClass('ui-scrollfix') && offset < attrs.uiScrollfix) {
+            fixLimit = limit;
+          } else if (elm.hasClass('ui-scrollfix') && offset < fixLimit) {
             elm.removeClass('ui-scrollfix');
           }
         }
