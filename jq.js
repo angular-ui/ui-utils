9dea8431ea52bf61a3ce69555833d07cc4fa566f
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-02-18 02:45:56 +0000

    Travis commit : build 422

--- jq.js
+++ jq.js
@@ -26,26 +26,29 @@ angular.module('ui.jq', []).value('uiJqConfig', {}).directive('uiJq', [
         }
         var options = uiJqConfig && uiJqConfig[tAttrs.uiJq];
         return function uiJqLinkingFunction(scope, elm, attrs) {
-          var linkOptions = [];
-          // If ui-options are passed, merge (or override) them onto global defaults and pass to the jQuery method
-          if (attrs.uiOptions) {
-            linkOptions = scope.$eval('[' + attrs.uiOptions + ']');
-            if (angular.isObject(options) && angular.isObject(linkOptions[0])) {
-              linkOptions[0] = angular.extend({}, options, linkOptions[0]);
-            }
-          } else if (options) {
-            linkOptions = [options];
-          }
           // If change compatibility is enabled, the form input's "change" event will trigger an "input" event
           if (attrs.ngModel && elm.is('select,input,textarea')) {
             elm.bind('change', function () {
               elm.trigger('input');
             });
           }
+          function createLinkOptions() {
+            var linkOptions = [];
+            // If ui-options are passed, merge (or override) them onto global defaults and pass to the jQuery method
+            if (attrs.uiOptions) {
+              linkOptions = scope.$eval('[' + attrs.uiOptions + ']');
+              if (angular.isObject(options) && angular.isObject(linkOptions[0])) {
+                linkOptions[0] = angular.extend({}, options, linkOptions[0]);
+              }
+            } else if (options) {
+              linkOptions = [options];
+            }
+            return linkOptions;
+          }
           // Call jQuery method and pass relevant options
           function callPlugin() {
             $timeout(function () {
-              elm[attrs.uiJq].apply(elm, linkOptions);
+              elm[attrs.uiJq].apply(elm, createLinkOptions());
             }, 0, false);
           }
           // If ui-refresh is used, re-fire the the method upon every change
