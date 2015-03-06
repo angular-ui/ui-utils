2e2375003d2bf31a5494f1239bf23c930aa84ec5
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-03-06 21:14:20 +0000

    Travis commit : build 433

--- route.min.js
+++ route.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.2.2 - 2015-02-18
+ * @version v0.2.2 - 2015-03-06
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
