05ddc8578b484ed8f1a318177dd6f722e7d0223e
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-05-07 08:23:14 +0000

    Travis commit : build 450

--- indeterminate.min.js
+++ indeterminate.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.2.3 - 2015-05-06
+ * @version v0.2.3 - 2015-05-07
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
