d0ab9e2c390f6d01d6e6dd8ca61c7ffd2625f43a
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-05-22 13:16:05 +0000

    Travis commit : build 456

--- event.min.js
+++ event.min.js
@@ -1,6 +1,6 @@
 /**
  * angular-ui-utils - Swiss-Army-Knife of AngularJS tools (with no external dependencies!)
- * @version v0.2.3 - 2015-05-19
+ * @version v0.2.3 - 2015-05-22
  * @link http://angular-ui.github.com
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
