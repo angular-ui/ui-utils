a23c3129c159204b3a7ac93e50daf48408c5dabd
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2015-06-30 03:50:47 +0000

    Travis commit : build 468

--- dist/ui-utils.js
+++ dist/ui-utils.js
@@ -893,11 +893,13 @@ angular.module('ui.mask', [])
             if (input.selectionStart !== undefined) {
               return input.selectionStart;
             } else if (document.selection) {
-              // Curse you IE
-              input.focus();
-              var selection = document.selection.createRange();
-              selection.moveStart('character', input.value ? -input.value.length : 0);
-              return selection.text.length;
+              if (iElement.is(':focus')) {
+                // Curse you IE
+                input.focus();
+                var selection = document.selection.createRange();
+                selection.moveStart('character', input.value ? -input.value.length : 0);
+                return selection.text.length;
+              }
             }
             return 0;
           }
@@ -908,16 +910,18 @@ angular.module('ui.mask', [])
               return; // Input's hidden
             }
             if (input.setSelectionRange) {
-              input.focus();
-              input.setSelectionRange(pos, pos);
+              if (iElement.is(':focus')) {
+                input.focus();
+                input.setSelectionRange(pos, pos);
+              }
             }
             else if (input.createTextRange) {
-              // Curse you IE
-              var range = input.createTextRange();
-              range.collapse(true);
-              range.moveEnd('character', pos);
-              range.moveStart('character', pos);
-              range.select();
+                // Curse you IE
+                var range = input.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
             }
           }
 
